6c6924d9b35596e41787602a2e99d47853072a78
Ivan Nawrath <inawrath@informaticos.com>
2012-06-20 15:29:40 -0400

    configuraciones

--- variablesDeConfiguracion.php
+++ variablesDeConfiguracion.php
@@ -1,18 +1,32 @@
 <?php
 $config = config::singleton();
 
-/*Base de Datos compartida*/
-/*$config->guardarVariable('dbhost', 'lacubb.sytes.net');
-$config->guardarVariable('dbname', 'lac');
-$config->guardarVariable('dbuser', 'jona');
-$config->guardarVariable('dbpass', 'jerusalen22');//*/
+/*Base de Datos ORACLE*/
+$config->guardarVariable('driver', 'oci');
+$config->guardarVariable('dbhost', 'localhost');
+$config->guardarVariable('dbname', 'XE');
+$config->guardarVariable('dbuser', 'SYSTEM');
+$config->guardarVariable('dbpass', 'oracle');
+//*/
 
 /*base de datos de pruebas*/
+/*$config->guardarVariable('driver', 'mysql');
 $config->guardarVariable('dbhost', 'localhost');
 $config->guardarVariable('dbname', 'prueba');
 $config->guardarVariable('dbuser', 'root');
-$config->guardarVariable('dbpass', '');//*/
+$config->guardarVariable('dbpass', '');
+//*/
 
+//PUERTO SOLO PARA ORACLE
+$config->guardarVariable('port', '1521');
+
+//MODO DEBUG ON
+$config->guardarVariable('options', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+//*/
+
+//MODO DEBUG OFF
+//$config->guardarVariable('options', array());
+//*/
 
 /*url principal de la pagina por ahora es http://localhost/paulo despues pasara a ser http://www.dominio.algo*/
 $config->guardarVariable('url','http://localhost/paulo/');
