1cb79364d29563e26ca3c6e4cdb832db01e281d2
Ivan Nawrath <inawrath@informaticos.com>
2012-06-25 00:07:12 -0400

    asdfg

--- vistas/inicio.php
+++ vistas/inicio.php
@@ -8,9 +8,10 @@
     while ($item = $listado->fetch()) {
         ?>
         <tr>
-            <td><?= $item['nombre'] ?></td><td><?= $item['tipo'] ?></td><td><?= $item['resumen'] ?></td>
+            <td><?= $item[0] ?></td><td><?= $item[1] ?></td><td><?= $item[2] ?></td>
         </tr>
         <?php
     }
     ?>
 </table>
+
